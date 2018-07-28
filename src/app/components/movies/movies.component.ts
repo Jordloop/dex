70a4ebe0be2fe7ee760412f2ca4af8d25af0fa5e
JordLoop <Jordloop@gmail.com>
2018-07-28 14:19:37 -0700

    add seeMovieDetails and addMovieToCollection

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { omdbapi } from './../../../environments/keys';
+import { OmdbapiService } from './../../services/omdbapi.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movies',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent implements OnInit {
+  @Input() moviesList;
 
-  constructor() { }
+  constructor(
+    private omdbSvc: OmdbapiService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    console.log('movies, OnInit:', this.moviesList);
+    
   }
+
+  seeMovieDetails(imdbID: string) {
+    this.omdbSvc.getMovieByImdbId(imdbID).subscribe(movie => {
+      if(movie) {
+        const temp = JSON.parse(movie._body);
+        console.log('get by imdbID:', temp);
+      }
+    })
+  }
+
+  addMovieToCoollection(imdbID: string) {
+    console.log(imdbID);
+    this.seeMovieDetails(imdbID);
+  }
+
+  navigateToMovieDetail(imdbID: string) {
+    this.router.navigate([`movie/${imdbID}`]);
+  }
+
+
+
 
 }
