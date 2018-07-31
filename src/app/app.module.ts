0a8be59683ce5c2eb4ed09a9baaa2ee00a54336d
JordLoop <Jordloop@gmail.com>
2018-07-30 17:44:58 -0700

    import firebase libs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
+import { MovieServiceService } from './services/movie-service.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { routing } from './app.routing';
 
+import { masterFirebaseConfig } from '../environments/keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { FirebaseApp } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -14,6 +19,14 @@ import { MovieDetailComponent } from './components/movie-detail/movie-detail.com
 import { MovieEditComponent } from './components/movie-edit/movie-edit.component';
 import { CollectionsComponent } from './components/collections/collections.component';
 import { CollectionsDetailComponent } from './components/collections-detail/collections-detail.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
+
 
 @NgModule({
   declarations: [
@@ -31,9 +44,13 @@ import { CollectionsDetailComponent } from './components/collections-detail/coll
     BrowserModule,
     HttpModule,
     FormsModule,
-    routing
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
   ],
-  providers: [],
+  providers: [
+    MovieServiceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
